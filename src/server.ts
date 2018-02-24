68865e058d34d1d79b7ac810bb4ffbfd196960fa
Dragan <d.novakovic1810@yahoo.com>
2018-02-24 08:41:46 +0100

    login init

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,7 @@ import { Request, Response, Application } from 'express'
 import routes from './routes/index'
 const app:Application = express()
 
-
+app.use(express.static(path.join(__dirname, '../client')));
 app.use('/', routes)
-
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
