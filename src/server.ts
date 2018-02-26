29688d98a2e207e06ee882ba857b20841f0110a7
Dragan <d.novakovic1810@yahoo.com>
2018-02-26 19:31:37 +0100

    bump

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import * as express from "express"
 import * as path from "path"
 import * as bodyParser from 'body-parser'
+import * as cookieParser from 'cookie-parser'
 import * as logger from 'morgan'
 import * as dotenv from 'dotenv'
 import * as mongoose from 'mongoose'
+import * as mongo from "connect-mongo";
 import * as expressValidator from 'express-validator'
+import * as session from 'express-session'
 import { Request, Response, Application } from 'express'
 import routes from './routes/index'
 
+const MongoStore = mongo(session)
 
 const app:Application = express()
 
@@ -26,7 +30,17 @@ app.use(logger('dev'))
 app.use(express.static(path.join(__dirname, '../client')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(expressValidator())
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: "secret123",
+  store: new MongoStore({
+    url: "mongodb://127.0.0.1/node-boilerplate",
+    autoReconnect: true
+  })
+}));
 app.use('/', routes)
 
 app.listen(3000, () => console.log('PORT 3000! RUNING!'))
