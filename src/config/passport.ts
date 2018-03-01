0d6e371a7a0c75c8cbea1984742b7d3f624e6bf2
Dragan <d.novakovic1810@yahoo.com>
2018-03-01 16:00:03 +0100

    pass

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -19,7 +19,8 @@ passport.deserializeUser((id, done) => {
 /**
  * Sign in using Email and Password.
  */
-passport.use(new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+passport.use(new LocalStrategy(
+    { usernameField: "email" }, (email, password, done) => {
   User.findOne({ email: email.toLowerCase() }, (err, user: any) => {
     if (err) { return done(err); }
     if (!user) {
