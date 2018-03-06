de181e13b0361f70d2dbcf1dedda81e3484802cb
Dragan <d.novakovic1810@yahoo.com>
2018-03-06 13:43:33 +0100

    fresh

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,22 @@
-import * as express from "express"
-import * as path from "path"
-import * as bodyParser from 'body-parser'
-import * as cookieParser from 'cookie-parser'
-import * as logger from 'morgan'
-import * as dotenv from 'dotenv'
-import * as mongoose from 'mongoose'
-import * as mongo from "connect-mongo";
-import * as expressValidator from 'express-validator'
-import * as session from 'express-session'
-import * as passport from 'passport'
-import { Request, Response, Application } from 'express'
-import routes from './routes/index'
+import errorHandler from "errorhandler";
 
-const MongoStore = mongo(session)
+import app from "./app";
 
-const app:Application = express()
+/**
+ * Error Handler. Provides full stack - remove for production
+ */
+app.use(errorHandler());
 
-dotenv.config({ path: "../.env" });
-// Mongo
-//const mongoUrl:string = process.env.MONGOLAB_URI;
+/**
+ * Start Express server.
+ */
+const server = app.listen(app.get("port"), () => {
+  console.log(
+    "  App is running at http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env")
+  );
+  console.log("  Press CTRL-C to stop\n");
+});
 
-(<any>mongoose).Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1/node-boilerplate')
-    .then(() => { /** ready to use.The `mongoose.connect()` promise resolves to undefined. */ },)
-    .catch(err => { console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
-    // process.exit();
-  });
-
-app.use(logger('dev'))
-app.use(express.static(path.join(__dirname, '../client')))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cookieParser())
-app.use(expressValidator())
-// dokumentacija za sessije
-app.use(session({
-  name: "kolacic",
-  resave: true,
-  saveUninitialized: true,
-  secret: "secret123",
-  store: new MongoStore({
-    url: "mongodb://127.0.0.1/node-boilerplate",
-    autoReconnect: true
-  })
-}))
-app.use(passport.initialize());
-app.use(passport.session());
-app.use('/', routes)
-
-app.listen(3000, () => console.log('PORT 3000! RUNING!'))
+export default server;
--- src/test/api.test.ts
+++
@@ -1,9 +0,0 @@
-import * as request from "supertest";
-import * as app from "./server";
-
-describe("GET /api", () => {
-  it("should return 200 OK", () => {
-    return request(app).get("/api")
-      .expect(200);
-  });
-});
